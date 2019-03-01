ff72e9af0b04b109c569cc7921819c13f649aa21
Alex Somoza <ASomoza041@west-mec.org>
2019-02-28 17:13:40 -0700

    Final Commit
    Finished the for loop and added html to the cartHTML variable to close the table. Also added code to insert the table into the actual html document. Finally added comments to help explain my code. Ready for review.

--- tc_cart.js
+++ tc_cart.js
@@ -11,12 +11,22 @@
    Filename: tc_cart.js
 	
 */
-
+// The code below are global variable that can be used by another program in the code. The "orderTotal" variable sets up the beggining total for the order, and the "cartHTML" variable is the beggining of the code to create the table.
 var orderTotal = 0;
-var cartHTML = "<table> \n <tr> \n <th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th> \n </tr>";
+var cartHTML = "<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th>  </tr>";
+//The code below is creating a table that inputs all the variable given to it. The for loop runs through each array item, filling it in to each data cell.
+for (var i = 0; i <= 3; i++) {
+    cartHTML += "<tr>  <td><img src='tc_" + item[i] + ".png' alt='item' /></td>";
 
-for (item = 0; item <= 4; item++) {
-    cartHTML += "<tr> \n <td><img src='tc_" + item[i] +
-        ".png' alt='item' /></td>";
-    cartHTML += "<td>" + itemDescription[i] + "</td> \n <td> $" + itemPrice[i] +
+    cartHTML += "<td>" + itemDescription[i] + "</td>  <td> $" + itemPrice[i] + "</td>  <td>" + itemQty[i] + "</td>";
+
+    var itemCost = itemPrice[i] * itemQty[i];
+
+    cartHTML += "<td>$" + itemCost + "</td></tr>";
+
+    orderTotal = orderTotal + itemCost;
 }
+// The code below closes the table and inserts the table into hte html document.
+cartHTML += "<tr><td colspan='4'>Subtotal</td><td>$" + orderTotal + "</td></tr></table>";
+
+document.getElementById("cart").innerHTML = cartHTML;
